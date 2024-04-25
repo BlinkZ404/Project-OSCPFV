b109afbd97c0b34b5d2cbb5bf3d5067ab962a263
Arifur Rahman <arif.pcmr@gmail.com>
2024-04-25 17:25:08 +0600

    Item Model Added

--- oscp/product/models.py
+++ oscp/product/models.py
@@ -27,7 +27,31 @@ STATUS_CHOICES = (
     ('Canceled', 'Canceled')
 )
 class Item(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    variant = models.CharField(max_length=20, null=True)
+    details = models.TextField(max_length=500, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    image = models.ImageField(default='default.jpg', upload_to='static/images')
+    slug = models.SlugField()
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("product:product-page", kwargs={
+                       'slug': self.slug
+                       })
+
+    def get_add_to_cart_url(self):
+        return reverse("product:cart-page", kwargs={
+            'slug': self.slug
+        })
+
+    def img_tag(self):
+        return mark_safe(f'<img src="{self.image.url}" width="100" height="100" />')
+    img_tag.short_description = 'Image'
 
 class Merchant(models.Model):
     pass
